4ac16b6f07594d1f481782f2e3b03c591674623e
apropdelalluna <apropdelalluna@gmail.com>
2026-09-20 20:10:12 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 pip install -r requirements_artquemy.txt
-playwright install chromium
-playwright install-deps chromium
+playwright install chromium --with-deps 2>/dev/null || playwright install chromium
